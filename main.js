424486af9e233ae24593b1281315fd48645163a6
Noel <noel-kingsly.anupkumar@alumni.fh-aachen.de>
2018-03-01 01:05:43 +0100

    Changed Form for MainWindow. Hand over Form Parameters to main.js to execute Batch File.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 // SET ENV
-process.env.NODE_ENV = 'production';
+// process.env.NODE_ENV = 'production';
 
 let mainWindow;
 let checkWindow;
@@ -51,11 +51,58 @@ function createCheckWindow(){
     })
 }
 
+function checkUpdate(item){
+    "use strict";
+
+// The path to the .bat file
+    var myBatFilePath = "I:\\SQL-Skripte\\executeSQL.bat " + item.username + " " + item.password + " " + item.db ;
+
+    const spawn = require('child_process').spawn;
+    const bat = spawn('cmd.exe', ['/c', myBatFilePath]);
+
+
+// Handle normal output
+    bat.stdout.on('data', (data) => {
+        // As said before, convert the Uint8Array to a readable string.
+        var str = String.fromCharCode.apply(null, data);
+        console.info(str);
+    });
+
+// Handle error output
+    bat.stderr.on('data', (data) => {
+        // As said before, convert the Uint8Array to a readable string.
+        var str = String.fromCharCode.apply(null, data);
+        console.error(str);
+    });
+
+// Handle on exit event
+    bat.on('exit', (code) => {
+        var preText = `Child exited with code ${code} : `;
+
+        switch(code){
+            case 0:
+                console.info(preText+"Something unknown happened executing the batch.");
+                break;
+            case 1:
+                console.info(preText+"The file already exists");
+                break;
+            case 2:
+                console.info(preText+"The file doesn't exists and now is created");
+                break;
+            case 3:
+                console.info(preText+"An error ocurred while creating the file");
+                break;
+        }
+    });
+}
+
 // Catch item:add
-ipcMain.on('item:add', function(e, item){
-    console.log(item);
-    mainWindow.webContents.send('item:add', item);
-    checkWindow.close();
+ipcMain.on('item1:add', function(e, item){
+    // !!! Abfangen wenn item.username,password,db = null !!! -> validierung
+    //console.log(item.username);
+    checkUpdate(item)
+   // mainWindow.webContents.send('item:add', item);
+   // checkWindow.close();
 });
 
 //Create menu template
@@ -66,11 +113,8 @@ const mainMenuTemplate = [
             {
                 label: 'Check Updates',
                 click(){
-                    createCheckWindow();
+                    // createCheckWindow();
                 }
-            },
-            {
-                label: 'Move Updates'
             },
             {
                 label: 'Quit',
